3a55f8046201e551c2e963e705ab56a07c8b9f29
dylan <dylan.arias1205@gmail.com>
2026-03-30 15:04:28 -0300

    ej 1 completo

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -10,13 +10,15 @@ def contar_ciclo(n):
     """
     Retorna una lista con los números desde 1 hasta n usando iteración.
     """
-    # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
-
+    lista = []
+    for i  in range(n):
+        lista.append(i+1)
+    print(lista)
 
 def contar_recursivo(n):
     """
     Retorna una lista con los números desde 1 hasta n usando recursividad.
     """
-    # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
+    if (n == 1): return [1]
+    else: 
+        return contar_recursivo(n-1) + [n]
